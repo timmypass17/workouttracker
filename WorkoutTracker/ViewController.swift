2abe3096d67a8544cacf9bfe92a6b6da1b121050
Timmy Nguyen <timmypass17@gmail.com>
2023-07-25 19:50:57 -0700

    Initial Commit

---
+++ WorkoutTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorkoutTracker
+//
+//  Created by Timmy Nguyen on 7/25/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
