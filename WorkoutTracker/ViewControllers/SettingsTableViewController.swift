9e63518965d968c19484028afba281180462762f
Timmy Nguyen <timmypass17@gmail.com>
2023-08-20 15:37:25 -0700

    Added Help & Support email function

--- WorkoutTracker/ViewControllers/SettingsTableViewController.swift
+++ WorkoutTracker/ViewControllers/SettingsTableViewController.swift
@@ -6,12 +6,17 @@
 //
 
 import UIKit
+import SafariServices
+import MessageUI
 
-class SettingsTableViewController: UITableViewController, WeightUnitTableViewControllerDelegate, ThemeTableViewControllerDelegate {
+class SettingsTableViewController: UITableViewController, WeightUnitTableViewControllerDelegate, ThemeTableViewControllerDelegate, MFMailComposeViewControllerDelegate {
     
     @IBOutlet var weightTypeLabel: UILabel!
     @IBOutlet var themeLabel: UILabel!
     
+    let contactUsIndexPath = IndexPath(row: 0, section: 2)
+    let bugReportIndexPath = IndexPath(row: 1, section: 2)
+
     enum Section: Int, CaseIterable {
         case units
         case appearance
@@ -65,5 +70,41 @@ class SettingsTableViewController: UITableViewController, WeightUnitTableViewCon
     
     func themeTableViewController(_ controller: ThemeTableViewController, didSelect themeType: Theme) {
         updateView()
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath == contactUsIndexPath {
+            guard MFMailComposeViewController.canSendMail() else {
+                // Disable button or show error message
+                print("Can not send mail")
+                return
+            }
+            
+            let mailComposer = MFMailComposeViewController()
+            mailComposer.mailComposeDelegate = self
+            mailComposer.setToRecipients(["timmypass17@gmail.com"]) // change to support email
+            mailComposer.setSubject("Contact Us")
+            
+            present(mailComposer, animated: true)
+        } else if indexPath == bugReportIndexPath {
+            guard MFMailComposeViewController.canSendMail() else {
+                return
+            }
+            
+            let mailComposer = MFMailComposeViewController()
+            mailComposer.mailComposeDelegate = self
+            
+            mailComposer.setToRecipients(["timmypass17@gmail.com"]) // change to support email
+            mailComposer.setSubject("Bug Report")
+            
+            present(mailComposer, animated: true)
+        }
+    }
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        dismiss(animated: true)
+        if let selectedIndexPath = tableView.indexPathForSelectedRow {
+            tableView.deselectRow(at: selectedIndexPath, animated: true)
+        }
     }
 }
